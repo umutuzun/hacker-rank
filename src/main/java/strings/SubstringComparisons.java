d690c877402ef569cc2fa8a799d4588514f265a0
Umut Uzun <ucuzun@gmail.com>
2018-12-24 11:11:30 +0300

    Add project

---
+++ src/main/java/strings/SubstringComparisons.java
@@ -0,0 +1,27 @@
+package strings;
+
+import java.util.Scanner;
+
+public class SubstringComparisons {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
