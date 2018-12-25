1dcb206fa874ceebf5c715b6230db40665b72306
Umut Uzun <ucuzun@gmail.com>
2018-12-25 15:44:05 +0300

    Add Regex

---
+++ src/main/java/strings/Regex.java
@@ -0,0 +1,23 @@
+package strings;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Regex{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+
+    static class MyRegex {
+        String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+
+
+    }
+}
