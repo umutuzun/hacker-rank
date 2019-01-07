43dc27bac79b3b803e1d0c868783a5f0b6e307fc
Umut Uzun <ucuzun@gmail.com>
2019-01-07 10:53:46 +0300

    Add MiniMaxSum

---
+++ src/main/java/algorithms/warmup/MiniMaxSum.java
@@ -0,0 +1,44 @@
+package algorithms.warmup;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class MiniMaxSum {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+        long mid = 0;
+        Arrays.sort(arr);
+        for (int i=1; i < arr.length-1; i++) {
+            mid += arr[i];
+        }
+        long min = mid + arr[0];
+        long max = mid + arr[arr.length-1];
+
+        System.out.println(min + " " + max);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
